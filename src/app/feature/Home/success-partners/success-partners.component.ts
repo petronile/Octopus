f06ba1987483ba1576fa7565ae7455569b49e0f5
petronile <127682580+Peterolltek@users.noreply.github.com>
2023-06-17 14:39:22 +0300

    to-trust

--- src/app/feature/Home/success-partners/success-partners.component.ts
+++ src/app/feature/Home/success-partners/success-partners.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class SuccessPartnersComponent {
 
+  partners: any[];
+
+  constructor() {
+    this.partners = [
+      {
+        img:"img-shell"
+      },
+      {
+        img:"img-valvo"
+      },
+      {
+        img:"img-castrol"
+      },
+      {
+        img:"img-valvo"
+      },
+      {
+        img:"img-castrol"
+      },
+    ];
+  }
 }
